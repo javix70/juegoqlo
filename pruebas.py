62f7fdc6105d31132fa0fa68be0f67243e960106
javier <javier17omar@gmail.com>
2020-03-24 17:04:14 -0300

    funciones

--- pruebas.py
+++ pruebas.py
@@ -6,6 +6,7 @@ import pytesseract
 from pytesseract import Output
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+
 import glob
 import pyautogui
 import csv
